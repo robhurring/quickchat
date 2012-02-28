b052b3bc8469025dd7518a8d745b1bee31f4646f
Rob <robhurring@gmail.com>
2012-02-28 17:32:17 -0500

    adding providers to embed and fixing regex

--- lib/oembed.rb
+++ lib/oembed.rb
@@ -1,14 +1,20 @@
 class OEmbed
   Providers = [
-    'www.youtube.com',
-    'www.flickr.com',
-    'vimeo.com'
+    /youtube.com\/watch.+/,
+    /flickr\.com\/photos/,
+    /viddler\.com/,
+    /blip\.tv\/.+/,
+    /hulu\.com\/watch.*/,
+    /vimeo\.com/,
+    /dailymotion\.com\/.+/,
+    /scribd\.com\/.+/,
+    /slideshare\.net/,
+    /photobucket\.com\/(?:albums|groups)/
   ]
 
   class << self
     def regex
-      hosts = Providers.map{ |host| Regexp.escape host }.join '|'
-      %r{(?:https?://)(#{hosts})(?:\/(?:\S+))}
+      %r{(?:https?://)?((?:www\.)?#{Providers.join '|'})}
     end
   end
 end
